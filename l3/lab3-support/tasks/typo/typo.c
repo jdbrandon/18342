3276ed2ae9a307b745d8d450454b03254e555ca3
Keane Lucas <keenlooks@gmail.com>
2014-11-03 18:28:44 -0500

    working on typo

--- l3/lab3-support/tasks/typo/typo.c
+++ l3/lab3-support/tasks/typo/typo.c
@@ -1,12 +1,24 @@
 /** @file typo.c
  *
- * @brief Echos characters back with timing data.
+ * @brief Displays a spinning cursor.
  *
  * Links to libc.
  */
 
+#include <unistd.h>
+
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
-	return 0;
+	char rawinput[100];
+	unsigned starttime = 0;
+	unsigned length = 0;
+	while(1){
+		write(STDOUT_FILENO, ">>>", 3);
+		starttime = time();
+		length = read(STDIN_FILENO, rawinput, 100);
+		write(STDOUT_FILENO, rawinput, length);
+		write(STDOUT_FILENO, ((time() - starttime)/1000), 3);
+		write(STDOUT_FILENO, "s", 3);
+	}
+	return 0
 }

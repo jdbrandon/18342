ba8b6b970ec34ab00e0b2718678bbbe835a3ae6b
Keane Lucas <keenlooks@gmail.com>
2014-11-04 06:38:56 -0500

    better typehero

--- l3/lab3-support/tasks/typehero/typehero.c
+++ l3/lab3-support/tasks/typehero/typehero.c
@@ -1,9 +1,9 @@
 #define TIMER_BASE_ADDR 0x40a00000
 #define OSCR (TIMER_BASE_ADDR + 0x10) /* OS Timer counter register */
-#define STEP 1000
 
 #include <unistd.h>
 #include <stdio.h>
+#include <bits/swi.h>
 
 typedef volatile unsigned * mmio_t;
 
@@ -22,9 +22,12 @@ int main(int argc, char** argv){
 	printf("GO!\n");
 	unsigned random = 0;
 	unsigned start_time = 0;
+	int step = 1000;
 	int diff = 0;
 	int score = 0;
 	int hit = 0;
+	int highscore = 0;
+	int streak = 0;
 	while(1){
 		random = *oscr % 8;
 		start_time = time();
@@ -33,7 +36,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='a'){
+			if(check[0]=='a' || check[0]=='A'){
 				hit = 1;
 			}
 		}else if(random == 1){
@@ -41,7 +44,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='s'){
+			if(check[0]=='s' || check[0]=='S'){
 				hit = 1;
 			}
 		}else if(random == 2){
@@ -49,7 +52,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='d'){
+			if(check[0]=='d' || check[0]=='D'){
 				hit = 1;
 			}
 		}else if(random == 3){
@@ -57,7 +60,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='f'){
+			if(check[0]=='f' || check[0]=='F'){
 				hit = 1;
 			}
 		}else if(random == 4){
@@ -65,7 +68,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='j'){
+			if(check[0]=='j' || check[0]=='J'){
 				hit = 1;
 			}
 		}else if(random == 5){
@@ -73,7 +76,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='k'){
+			if(check[0]=='k' || check[0]=='K'){
 				hit = 1;
 			}
 		}else if(random == 6){
@@ -81,7 +84,7 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]=='l'){
+			if(check[0]=='l' || check[0]=='L'){
 				hit = 1;
 			}
 		}else if(random == 7){
@@ -89,17 +92,33 @@ int main(int argc, char** argv){
 			read(STDIN_FILENO, &check, 1);
 			printf("\b \b");
 			diff = time() - start_time;
-			if(check[0]==';'){
+			if(check[0]==';' || check[0]==':'){
 				hit = 1;
 			}
 		}
-		if(diff < STEP && hit){
+		if(diff < step && hit){
 			printf("   HIT!    Score: %d +1\n", score);
 			score++;
+			streak++;
+			if(score > highscore){
+				highscore = score;
+			}
+			if(streak > 9){
+				printf("WHOA! 10 HIT streak! +5 Points! Making this harder... better get faster...\n");
+				streak = 0;
+				score += 5;
+				step -= 100;
+			}
 		}else{
-			score -= (diff/STEP + 1);
-			printf("   miss... Score: %d -%d\n", score, diff/STEP+1);
+			score -= (diff/step + 1);
+			printf("   miss... Score: %d -%d\n", score, diff/step+1);
+			streak = 0;
+			if(score < -49){
+				printf("Nice try... Better luck next time. Highest Score: %d\n",highscore);
+				break;
+			}
 		}
 		hit = 0;
 	}
+	return 0;
 }

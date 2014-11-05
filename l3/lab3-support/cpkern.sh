eb128b552955748fecd66ddd3e747cb27ed357d6
Jeff Brandon <jeffbrandon2010@gmail.com>
2014-11-05 18:13:02 -0500

    modify test func

---
+++ l3/lab3-support/cpkern.sh
@@ -0,0 +1,3 @@
+sudo cp ./kernel/kernel.bin /media/bootfs;
+sudo cp ./tasks/bin/*.bin /media/bootfs;
+sync;
--- l3/lab3-support/tasks/test/test.c
+++ l3/lab3-support/tasks/test/test.c
@@ -3,15 +3,12 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <ctype.h>
 
 typedef volatile unsigned * mmio_t;
 
 int main(int argc, char** argv){
-	mmio_t oscr = (mmio_t) OSCR;
-	*oscr = 0xffffff00;
+//	mmio_t oscr = (mmio_t) OSCR;
+//	*oscr = 0xffffff00;
 	while(1){
 		printf("time:%ld\n", time());
 		sleep(200);

4bddff507dc8a973845f17e7482fe7720000a815
Keane Lucas <keenlooks@gmail.com>
2014-11-17 21:31:45 -0500

    stuff and things

--- l4/lab4/kernel/syscall/proc.c
+++ l4/lab4/kernel/syscall/proc.c
@@ -22,9 +22,38 @@
 #include <arm/physmem.h>
 #include <device.h>
 
-int task_create(task_t* tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
+extern tcb_t system_tcb[OS_MAX_TASKS];
+extern void runqueue_init(void);
+extern void runqueue_add(tcb_t*, uint8_t);
+extern void idle(void);
+extern void sched_init(task_t*);
+extern void allocate_tasks(task_t**, size_t);
+
+int task_create(task_t* tasks, size_t num_tasks)
 {
-  return 1; /* remove this line after adding your code */
+	size_t i;
+	task_t* best = NULL;
+	unsigned long tempT = 0, mintime = 0 ,maxtime = 0;
+	runqueue_init();
+	for(i = 0; i<num_tasks; i++){
+		tempT = tasks[i].T;
+		if(tempT < mintime || mintime == 0){
+			mintime = tempT;
+			best = &tasks[i];
+		}
+		if(tempT > maxtime || maxtime == 0){
+			maxtime = tempT;
+		}
+	}
+	for(i = 0; i<num_tasks; i++){
+		runqueue_add(&system_tcb[i], (uint8_t)(1+((tasks[i].T-mintime)/(maxtime-mintime))*62)); //if something goes wrong DEBUG ME
+	}
+	//add idle queue
+	//set up the link register
+
+	sched_init(best);
+//should never get to this point
+  return 0; /* remove this line after adding your code */
 }
 
 int event_wait(unsigned int dev  __attribute__((unused)))

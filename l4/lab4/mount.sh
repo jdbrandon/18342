69c91b56d1ec922dab8c4ff3e5d727b914f0fa8a
Jeff Brandon <jeffbrandon2010@gmail.com>
2014-11-23 17:53:18 -0500

    changing mount script

--- l4/lab4/mount.sh
+++ l4/lab4/mount.sh
@@ -1,9 +1,3 @@
 #! /bin/sh
-echo "making"
-make
-echo "mounting mmc1"
-mount /mnt/mmc1
-cp kernel/kernel.bin /mnt/mmc1
-cp tasks/bin/*.bin /mnt/mmc1
-umount /mnt/mmc1
-echo "done mounting"
+sudo cp kernel/kernel.bin /media/bootfs
+sudo cp tasks/bin/*.bin /media/bootfs

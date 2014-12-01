5ca3868c044275c09d29b027ac1cdb150b6ee19a
Keane Lucas <keenlooks@gmail.com>
2014-11-30 20:31:34 -0500

    blablba

--- l4/lab4/kernel/sched/ub_test.c
+++ l4/lab4/kernel/sched/ub_test.c
@@ -9,11 +9,16 @@
 //#define DEBUG 0
 
 #include <sched.h>
+#include <exports.h>
 #ifdef DEBUG
 #include <exports.h>
 #endif
 
+void sorttasks(task_t*, size_t);
+void swap(task_t*, task_t*);
  
+double kroottwo[64] = {2.0, 1.4142135623730951, 1.2599210498948732, 1.189207115002721, 1.148698354997035, 1.122462048309373, 1.1040895136738123, 1.0905077326652577, 1.080059738892306, 1.0717734625362931, 1.0650410894399627, 1.0594630943592953, 1.0547660764816467, 1.0507566386532194, 1.0472941228206267, 1.0442737824274138, 1.0416160106505838, 1.0392592260318434, 1.0371550444461919, 1.0352649238413776, 1.0335577830070277, 1.0320082797342096, 1.0305955447520094, 1.029302236643492, 1.0281138266560665, 1.0270180507087725, 1.0260044847070386, 1.0250642119658746, 1.0241895602489972, 1.023373891996775, 1.0226114356012683, 1.0218971486541166, 1.021226606315364, 1.0205959095795867, 1.020001609421199, 1.0194406437021448, 1.0189102844052507, 1.018408093274102, 1.0179318843373906, 1.0174796921026863, 1.0170497444436268, 1.0166404393919355, 1.016250325193552, 1.0158780831055514, 1.015522512504275, 1.0151825179503484, 1.0148570979170202, 1.0145453349375237, 1.0142463869673273, 1.013959479790029, 1.0136839003226856, 1.0134189906987003, 1.0131641430249148, 1.0129187947249467, 1.0126824243937012, 1.012454548098765, 1.0122347160734744, 1.012022509754105, 1.0118175391201212, 1.0116194403019225, 1.0114278734252204, 1.0112425206651807, 1.0110630844868955, 1.0108892860517005};
+
 /**
  * @brief Perform UB Test and reorder the task list.
  *
@@ -27,11 +32,77 @@
  * @return 0  The test failed.
  * @return 1  Test succeeded.  The tasks are now in order.
  */
-int assign_schedule(task_t** tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
+int assign_schedule(task_t** tasksptr, size_t num_tasks)
 {
-
-	return 1; // fix this; dummy return to prevent compiler warnings	
+	task_t* tasks = *tasksptr;
+	sorttasks(tasks, num_tasks);
+	size_t i;
+	size_t j;
+	double util = 0.0;
+	for(i=0; i<num_tasks; i++){
+		util = 0.0;
+		for(j=0; j<i; j++){
+			util += (double)tasks[j].C/(double)tasks[j].T;
+			printf("utilloop: %d\n", (int)(util));
+		}
+		util += ((double)tasks[i].C + (double)tasks[i].B);
+		util /= (double)tasks[i].T;
+		if(util > ((double)i+1.0)*((double)kroottwo[i]-1.0)){
+			printf("utilfail: %d\n", (int)(util*100000));
+			return 0;
+		}
+	}
+	printf("utildone: %d\n", (int)util*100000);
+	return 1;
 }
 	
+/**
+ * @brief Method to sort tasks by their frequency T
+ *
+ * @param tasks  List of task structs
+ * @param count  Size of list
+ */
 
+void sorttasks(task_t* tasks, size_t count){
+	size_t i, j, min = 0, minpos = 0;
+	for(i = 0; i < (count-1); i++){
+		min = tasks[i].T;
+		minpos = i;
+		for(j = i+1; j < count; j++){
+			if(tasks[j].T < min){
+				min = tasks[j].T;
+				minpos = j;
+			}
+		}
+		if(i != minpos)
+			swap(&tasks[i], &tasks[minpos]);
+	}
+}
+
+/**
+ * @brief Method used to swap two tasks
+ *
+ * param a  Task 1
+ * param b  Task 2
+ */
+void swap(task_t* a, task_t* b){
+	task_t tmp;
+	tmp.lambda = a->lambda;
+	tmp.data = a->data;
+	tmp.stack_pos = a->stack_pos;
+	tmp.C = a->C;
+	tmp.T = a->T;
+	
+	a->lambda = b->lambda;
+	a->data = b->data;
+	a->stack_pos = b->stack_pos;
+	a->C = b->C;
+	a->T = b->T;
+
+	b->lambda = tmp.lambda;
+	b->data = tmp.data;
+	b->stack_pos = tmp.stack_pos;
+	b->C = tmp.C;
+	b->T = tmp.T;
+}
 
